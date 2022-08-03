58e13d6fcefabfc0be931b24527c63b3fe42408c
yutabee <bsk87m@gmail.com>
2022-08-03 12:24:07 +0900

    firebase connect

--- src/pages/Column.jsx
+++ src/pages/Column.jsx
@@ -1,38 +1,100 @@
-import {useState,} from 'react';
+import {useEffect, useState,} from 'react';
 import {css} from '@emotion/react';
 import {Box} from '../components/atoms';
 import { PlusIcon } from '../assets/icon';
 import ColumnItem from '../components/column/ColumnItem';
+import {db} from '../firebase';
+import {
+	collection,
+  doc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  getDocs,
+  serverTimestamp,
+  query,
+  orderBy,
+} from 'firebase/firestore';
 
 const Column = () => {
   const MAX_COLUMN_COUNT = 3;
   const [columns, setColumns] = useState([]);
 
-  const addColumn = () => {
-    if (columns.length < MAX_COLUMN_COUNT) {
-      const id = Math.random().toString(32).substring(2);
-      const column = {columnId: id, query: ''};
+  const addColumn = async () => {
+	try {
+	  if (columns.length < MAX_COLUMN_COUNT) {
+      const data = {
+        query: '',
+        created_at: serverTimestamp(),
+      };
+			const columnCollectionRef = collection(db, 'columns');
+      const colmnRef = await addDoc(columnCollectionRef, data);
 
-      setColumns([...columns, column]);
+      setColumns([...columns, {...data, columnId: colmnRef.id}]);
     }
-  };
+  } catch (e) {
+    console.log(e);
+  }
+};
     
-    const handleSearch = (id, query) => {
+    const handleSearch = async (id, query) => {
+  try {
+		const columnCollectionRef = collection(db, 'columns');
+    const docRef = doc(columnCollectionRef, id);
+    await updateDoc(docRef, {query});
+
     const newColmns = [...columns];
     const index = newColmns.findIndex(val => val.columnId === id);
     newColmns[index].query = query;
-
     setColumns(newColmns);
-    };
+  } catch (e) {
+    console.log(e);
+  }
+};
     
-const removeColumn = columnId => {
-  if (columns.length) {
-    const removed = columns.filter(col => col.columnId !== columnId);
+const removeColumn = async (id) => {
+  try {
+    if (columns.length) {
+			const columnCollectionRef = collection(db, 'columns');
+      const docRef = doc(columnCollectionRef, id);
+      await deleteDoc(docRef);
 
-    setColumns(removed);
+      const removed = columns.filter(col => col.columnId !== id);
+      setColumns(removed);
+    }
+  } catch (e) {
+    console.log(e);
   }
 };
 
+  
+const getColumnDocs = async () => {
+  try {
+		const columnCollectionRef = collection(db, 'columns');
+    const q = query(columnCollectionRef, orderBy('created_at', 'asc'));
+    const querySnapshot = await getDocs(q);
+
+    const newColmns = querySnapshot.docs.map(doc => {
+      const docData = doc.data();
+
+      return {
+        columnId: doc.id,
+        query: docData.query,
+        created_at: docData.created_at,
+      };
+    });
+
+    setColumns(newColmns);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+useEffect(() => {
+  getColumnDocs();
+// eslint-disable-next-line
+}, []);
+  
   return (
     <Box css={contaienr}>
       <Box css={columnWrap}>
